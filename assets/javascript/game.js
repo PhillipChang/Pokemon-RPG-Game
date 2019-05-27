042ed0b35113cd572251b419a2131d97ab68e0a8
Phillip Chang <phillip-chang@live.com>
2019-05-27 15:42:11 -0700

    User can pick character into selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,10 @@ audioElement.setAttribute("src", "assets/music/pokemon-song.MP3");
 $(".btn-start").on("click", function() {
     $(".main-screen").fadeOut();
     $(".loading-screen").fadeIn();
-    $(".loading-screen").delay(5000).fadeOut();
-    $(".character-screen").delay(7000).fadeIn();
+    $(".loading-screen").delay(1000).fadeOut();
+    $(".character-screen").delay(1000).fadeIn();
+    // $(".loading-screen").delay(5000).fadeOut();
+    // $(".character-screen").delay(7000).fadeIn();
     audioElement.play();
 });
 });
@@ -21,48 +23,25 @@ $(".btn-battle").on("click", function() {
 });
 });
 
-
-
-
-
-
-
-// function fadeOut() {
-//    var fade = 1;
-//    var timer = setInterval(function() {
-//        if (fade <= 0.1) {
-//         clearInterval(timer);
-//         $(".loading-row").style.display = 'none';
-//        }
-//        $(".loading-row").style.opacity= fade;
-//        $(".loading-row").style.filter = fade*100;
-//        fade -= fade * 0.1;
-//    },50);
-// }
-   
-//    );
-
-// function frame() {
-//     if (load == 100) {
-//         clearInterval(timer);
-//         $(".loading-row").style.display = 'none';
-//     }
-//     else {
-//         load = load++;
-//     }
-// }
-// function fade(element) {
-//     var op = 1;  // initial opacity
-//     var timer = setInterval(function () {
-//         if (op <= 0.1){
-//             clearInterval(timer);
-//             element.style.display = 'none';
-//         }
-//         element.style.opacity = op;
-//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-//         op -= op * 0.1;
-//     }, 50);
-// }
+$(document).ready(function() {
+// var audioElement = document.createElement("audio");
+// audioElement.setAttribute("src", "assets/music/battle-song.MP3");
+$(".characters").on("click", function() {
+    if (pikachu) {
+        $("#pikachu").prependTo($(".user-char"));
+    }
+    if (squirtle) {
+        $("#squirtle").prependTo($(".user-char"));
+    }
+    if (charmander) {
+        $("#charmander").prependTo($(".user-char"));
+    }
+    if (mewtwo) {
+        $("#mewtwo").prependTo($(".user-char"));
+    }
+    
+});
+});
 
 
 
