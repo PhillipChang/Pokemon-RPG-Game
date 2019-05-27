2a0d7763119a89cdfbc133de19380c38acd431e3
Phillip Chang <phillip-chang@live.com>
2019-05-27 02:32:55 -0700

    Create fadein and out effect for loading screen from main screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,56 @@
+// // Main Page
 $(document).ready(function() {
 var audioElement = document.createElement("audio");
-audioElement.setAttribute("src", "assets/javascript/pokemon-song.MP3");
+audioElement.setAttribute("src", "assets/music/pokemon-song.MP3");
 $(".btn").on("click", function() {
+    $(".sec-2").fadeIn();
+    $(".sec-1").fadeOut();
+    $(".sec-3").fadeOut();
     audioElement.play();
-})
 });
+});
+
+
+
+
+
+
+// function fadeOut() {
+//    var fade = 1;
+//    var timer = setInterval(function() {
+//        if (fade <= 0.1) {
+//         clearInterval(timer);
+//         $(".loading-row").style.display = 'none';
+//        }
+//        $(".loading-row").style.opacity= fade;
+//        $(".loading-row").style.filter = fade*100;
+//        fade -= fade * 0.1;
+//    },50);
+// }
+   
+//    );
+
+// function frame() {
+//     if (load == 100) {
+//         clearInterval(timer);
+//         $(".loading-row").style.display = 'none';
+//     }
+//     else {
+//         load = load++;
+//     }
+// }
+// function fade(element) {
+//     var op = 1;  // initial opacity
+//     var timer = setInterval(function () {
+//         if (op <= 0.1){
+//             clearInterval(timer);
+//             element.style.display = 'none';
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op -= op * 0.1;
+//     }, 50);
+// }
 
 
 
