8bfb28bfef1b4db500f8be80cc76ea1b74704a61
Phillip Chang <phillip-chang@live.com>
2019-05-28 11:36:32 -0700

    Create character selection and enemy selection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,41 @@
-// // Main Page
+var pokemon = [
+    {
+        name: "Pikachu",
+        hp: 120,
+        attack: 25,
+        defend: 10,
+        image:"assets/images/pikachu-2.png",
+    },
+    {
+        name: "Squirtle",
+        hp: 100,
+        attack: 20,
+        defend: 15,
+        image:"assets/images/squirtle.png",
+    },
+    {
+        name: "Charmander",
+        hp: 100,
+        attack: 15,
+        defend: 25,
+        image:"assets/images/charmander-3.png",
+    },
+    {
+        name: "Mewtwo",
+        hp: 100,
+        attack: 30,
+        defend: 10,
+        image:"assets/images/mewtwo-2.png",
+    }];
+
+var userHp;
+var userAtk;
+var userPokemon;
+var enemyHp;
+var enemyAtk;
+var enemyPokemon;
+
+// Main Page
 $(document).ready(function() {
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/music/pokemon-song.MP3");
@@ -13,6 +50,49 @@ $(".btn-start").on("click", function() {
 });
 });
 
+
+// function enemyPokemon() {
+//     for (i=0;i<pokemon.length;i++){
+//         if (userPokemon !== pokemon[i]) {
+//             pokemon[i].prependTo($(".comp-char"));
+//         }
+//     }
+// }
+
+
+
+$(document).ready(function() {
+   $("#pikachu").on("click", function(){
+        $("#pikachu").appendTo($(".user-char"));
+        userPokemon = pokemon[0];
+        userHp = pokemon[0].hp;
+        userAtk = pokemon[0].attack;
+        $("#squirtle").appendTo($(".comp-char"));
+        $("#charmander").appendTo($(".comp-char"));
+        $("#mewtwo").appendTo($(".comp-char"));
+    });
+    $("#squirtle").on("click", function(){
+        $("#squirtle").appendTo($(".user-char"));
+        $("#pikachu").appendTo($(".comp-char"));
+        $("#charmander").appendTo($(".comp-char"));
+        $("#mewtwo").appendTo($(".comp-char"));
+    });
+    $("#charmander").on("click", function(){
+        $("#charmander").appendTo($(".user-char"));
+
+        $("#pikachu").appendTo($(".comp-char"));
+        $("#squirtle").appendTo($(".comp-char"));
+        $("#mewtwo").appendTo($(".comp-char"));
+    });
+    $("#mewtwo").on("click", function(){
+        $("#mewtwo").appendTo($(".user-char"));
+        $("#pikachu").appendTo($(".comp-char"));
+        $("#squirtle").appendTo($(".comp-char"));
+        $("#charmander").appendTo($(".comp-char"));
+    });
+    });
+
+
 $(document).ready(function() {
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/music/battle-song.MP3");
@@ -20,28 +100,11 @@ $(".btn-battle").on("click", function() {
     $(".character-screen").fadeOut();
     $(".battle-screen").fadeIn();
     audioElement.play();
+
 });
 });
 
-$(document).ready(function() {
-// var audioElement = document.createElement("audio");
-// audioElement.setAttribute("src", "assets/music/battle-song.MP3");
-$(".characters").on("click", function() {
-    if (pikachu) {
-        $("#pikachu").prependTo($(".user-char"));
-    }
-    if (squirtle) {
-        $("#squirtle").prependTo($(".user-char"));
-    }
-    if (charmander) {
-        $("#charmander").prependTo($(".user-char"));
-    }
-    if (mewtwo) {
-        $("#mewtwo").prependTo($(".user-char"));
-    }
-    
-});
-});
+
 
 
 
