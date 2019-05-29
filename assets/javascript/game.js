27a676a52defdb13e314cb94f4706956920632cd
Phillip Chang <phillip-chang@live.com>
2019-05-28 23:33:36 -0700

    Change character selection to include switch statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,14 @@
-var pokemon = [
-    {
-        name: "Pikachu",
-        hp: 120,
-        attack: 25,
-        defend: 10,
-        image:"assets/images/pikachu-2.png",
-    },
-    {
-        name: "Squirtle",
-        hp: 100,
-        attack: 20,
-        defend: 15,
-        image:"assets/images/squirtle.png",
-    },
-    {
-        name: "Charmander",
-        hp: 100,
-        attack: 15,
-        defend: 25,
-        image:"assets/images/charmander-3.png",
-    },
-    {
-        name: "Mewtwo",
-        hp: 100,
-        attack: 30,
-        defend: 10,
-        image:"assets/images/mewtwo-2.png",
-    }];
-
+$(document).ready(function() {
 var userHp;
 var userAtk;
-var userPokemon;
+var userDef;
 var enemyHp;
 var enemyAtk;
-var enemyPokemon;
+var enemyDef;
+var userChose = 0;
+var user;
 
 // Main Page
-$(document).ready(function() {
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/music/pokemon-song.MP3");
 $(".btn-start").on("click", function() {
@@ -48,63 +20,56 @@ $(".btn-start").on("click", function() {
     // $(".character-screen").delay(7000).fadeIn();
     audioElement.play();
 });
-});
 
+$(".character").on("click",function(){
+    if (userChose == 0) {
+        user = $(this).attr('id');
+        userChose = 1;
+        userHp = $(this).attr('data-health');
+        userAtk = $(this).attr('data-attack');
+        userDef = $(this).attr('data-defend');
+        $(this).appendTo(".user-char");
+        $(this).off('click');
+    switch (user) {
+        case 'pikachu':
+            $("#squirtle").appendTo(".comp-char-1");
+            $("#charmander").appendTo(".comp-char-2");
+            $("#mewtwo").appendTo(".comp-char-3");
+            break;
+        case 'squirtle':
+            $("#pikachu").appendTo(".comp-char-1");
+            $("#charmander").appendTo(".comp-char-2");
+            $("#mewtwo").appendTo(".comp-char-3");
+            break;
+        case 'charmander':
+           $("#pikachu").appendTo(".comp-char-1");
+           $("#squirtle").appendTo(".comp-char-2");
+           $("#mewtwo").appendTo(".comp-char-3");
+           break;
+        case 'mewtwo':
+           $("#pikachu").appendTo(".comp-char-1");
+           $("#squirtle").appendTo(".comp-char-2");
+           $("#charmander").appendTo(".comp-char-3");
+           break;
+    }
+    }
+   
+   else if (userChose == 1) {
+       enemy = $(this).attr('id');
+       userChose = 2;
+       enemyHp = $(this).attr('data-health');
+       enemyAtk = $(this).attr('data-attack');
+       enemyDef = $(this).attr('data-defend');
+       $(this).appendTo(".defender");
+       $(".btn-attack").on('click', attack);
+   }
+})
 
-// function enemyPokemon() {
-//     for (i=0;i<pokemon.length;i++){
-//         if (userPokemon !== pokemon[i]) {
-//             pokemon[i].prependTo($(".comp-char"));
-//         }
-//     }
-// }
-
-
-
-$(document).ready(function() {
-   $("#pikachu").on("click", function(){
-        $("#pikachu").appendTo($(".user-char"));
-        userPokemon = pokemon[0];
-        userHp = pokemon[0].hp;
-        userAtk = pokemon[0].attack;
-        $("#squirtle").appendTo($(".comp-char"));
-        $("#charmander").appendTo($(".comp-char"));
-        $("#mewtwo").appendTo($(".comp-char"));
-    });
-    $("#squirtle").on("click", function(){
-        $("#squirtle").appendTo($(".user-char"));
-        $("#pikachu").appendTo($(".comp-char"));
-        $("#charmander").appendTo($(".comp-char"));
-        $("#mewtwo").appendTo($(".comp-char"));
-    });
-    $("#charmander").on("click", function(){
-        $("#charmander").appendTo($(".user-char"));
-
-        $("#pikachu").appendTo($(".comp-char"));
-        $("#squirtle").appendTo($(".comp-char"));
-        $("#mewtwo").appendTo($(".comp-char"));
-    });
-    $("#mewtwo").on("click", function(){
-        $("#mewtwo").appendTo($(".user-char"));
-        $("#pikachu").appendTo($(".comp-char"));
-        $("#squirtle").appendTo($(".comp-char"));
-        $("#charmander").appendTo($(".comp-char"));
-    });
-    });
-
-
-$(document).ready(function() {
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/music/battle-song.MP3");
 $(".btn-battle").on("click", function() {
     $(".character-screen").fadeOut();
     $(".battle-screen").fadeIn();
     audioElement.play();
-
 });
 });
-
-
-
-
-
